98168941b8fe8ffb105598b38a584248a9abc3f8
ruaanlucass <lruan9440@gmail.com>
2021-12-09 21:30:05 -0300

    Finalização API

--- src/routes/tarefasRoute.js
+++ src/routes/tarefasRoute.js
@@ -4,5 +4,9 @@ const controller = require("../controller/tarefasController");
 
 router.get("/", controller.getAll);
 router.post("/", controller.postTarefa);
+router.get("/:id", controller.getById);
+router.delete("/id:1", controller.deleteTarefa);
+router.delete("/", controller.deleteTarefaConcluida);
+router.put("/:id", controller.putTarefa);
 
 module.exports = router;
